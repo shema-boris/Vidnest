e532fa49f852c9caa52e006c679d9c3224413e90
Shema Boris Benimana <sheboris123@gmail.com>
2025-08-28 16:58:39 -0500

    Reinstalled the node modules to remove version duplications

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,14 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ]
-    }
-  },
+  
   test: {
     environment: "jsdom",
     globals: true,
