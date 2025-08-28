834474adbbac2c9b0b7416ddf3df9ac553e8b4c8
Shema Boris Benimana <sheboris123@gmail.com>
2025-08-28 11:52:50 -0500

    Debugged some error

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ]
+    }
+  },
   test: {
-    environment: "jsdom",   // ✅ enables document/window
-    globals: true,          // ✅ allows describe/it without imports
-    setupFiles: "./src/setupTests.js", // optional setup file
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTests.js",
   },
 });
-
